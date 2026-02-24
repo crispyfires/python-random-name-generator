2fc1a0ee67a472235a28e3be646188036a9eccc4
Crispy <junkstuff7489@gmail.com>
2026-02-24 18:47:58 -0500

    gen: implement pl support

--- src/cli/gen.py
+++ src/cli/gen.py
@@ -2,6 +2,7 @@ import random
 
 import click
 
+import src.namelists.names_pl
 import src.namelists.names_uk
 
 
@@ -19,13 +20,20 @@ import src.namelists.names_uk
 )
 def generate_name(language, gender):
     if language.lower() == "random":
-        language = random.choice(["uk"])
+        language = random.choice(["pl", "uk"])
     if language.lower() == "uk":
         if gender.lower() == "m":
             name = src.namelists.names_uk.generate_name_m_uk()
             click.echo(name)
         elif gender.lower() == "f":
             name = src.namelists.names_uk.generate_name_f_uk()
+            click.echo(name)
+    elif language.lower() == "pl":
+        if gender.lower() == "m":
+            name = src.namelists.names_pl.generate_name_m_pl()
+            click.echo(name)
+        elif gender.lower() == "f":
+            name = src.namelists.names_pl.generate_name_f_pl()
             click.echo(name)
     else:
         click.echo("Language code not supported.")
